e38bad6a73a644f1d56cb748e9146c85651499a0
Rory Flint <roryflintphoto@gmail.com>
2022-06-22 10:44:52 +0100

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [modal, setModal] = useState<boolean>(false);
   const [input, setInput] = useState<string>("");
+  const [loading, setLoading] = useState<string | null>("loading");
 
   const toggleModal = () => {
     setModal(!modal);
@@ -41,6 +42,7 @@ function App() {
   };
 
   const addNewTodo = (input: string) => {
+    setLoading("posting");
     axios
       .post(
         "/.netlify/functions/addNewRecord",
@@ -55,8 +57,11 @@ function App() {
       .catch(() =>
         toast.error("Error creating task", { position: "bottom-center" })
       )
-      .finally(() => setInput(""));
-    setModal(false);
+      .finally(() => {
+        setInput("");
+        setModal(false);
+        setLoading(null);
+      });
   };
 
   const toggleCompleteState = async (id: string) => {
@@ -96,6 +101,7 @@ function App() {
       .get("/.netlify/functions/getRecords")
       .then((res) => {
         setTodos(res.data);
+        setLoading(null);
       })
       .catch(() =>
         toast.error("Error retrieving tasks", { position: "bottom-center" })
@@ -110,6 +116,17 @@ function App() {
       document.removeEventListener("keydown", closeModalWithEscapeKey, false);
   }, []);
 
+  if (loading === "loading") {
+    return (
+      <div
+        className="w-[600px] my-0 mx-auto 
+  mt-[50px] flex justify-center"
+      >
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div
       className="w-[600px] my-0 mx-auto 
@@ -120,6 +137,7 @@ function App() {
           input={input}
           setInput={setInput}
           addNewTodo={addNewTodo}
+          loading={loading}
         />
       )}
       <MenuArea
@@ -215,10 +233,12 @@ function NewTodoModal({
   input,
   setInput,
   addNewTodo,
+  loading,
 }: {
   input: string;
   setInput: Dispatch<SetStateAction<string>>;
   addNewTodo: (input: string) => void;
+  loading: string | null;
 }) {
   const handleForm = (e: any) => {
     e.preventDefault();
@@ -240,8 +260,9 @@ function NewTodoModal({
                   autoFocus
                 />
                 <button
-                  className="bg-blue-600 hover:bg-blue-800 border-blue-600 hover:border-blue-800 text-sm border-4 text-white py-1 px-6 rounded"
+                  className="bg-blue-600 hover:bg-blue-800 border-blue-600 hover:border-blue-800 text-sm border-4 text-white py-1 px-6 rounded disabled:opacity-20"
                   type="submit"
+                  disabled={loading === "posting"}
                 >
                   Add
                 </button>
@@ -252,5 +273,26 @@ function NewTodoModal({
       </div>
       <div className="opacity-50 fixed inset-0 z-40 bg-black"></div>
     </>
+  );
+}
+
+function Spinner() {
+  return (
+    <svg
+      role="status"
+      className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-blue-200 fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
   );
 }
