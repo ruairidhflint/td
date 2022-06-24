833b2055f7f081ed61214a70f283f021561faf03
Rory Flint <roryflintphoto@gmail.com>
2022-06-24 15:55:00 +0100

    Add scheduled deletion function

---
+++ functions/scheduledDeletion.ts
@@ -0,0 +1,33 @@
+const { schedule, Handler } = require("@netlify/functions");
+import { base } from "../config/airtable";
+
+const handler = async (): Promise<{
+  statusCode: 200 | 500;
+  body?: string;
+}> => {
+  try {
+    const records = await base("Table 1")
+      .select({
+        filterByFormula: "NOT({status} = 'active')",
+        sort: [{ field: "created_at", direction: "desc" }],
+      })
+      .firstPage();
+
+    const ids = records.map((record) => record.id);
+
+    for (const id of ids) {
+      await base("Table 1").destroy(id);
+    }
+
+    return {
+      statusCode: 200,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  }
+};
+
+module.exports.handler = schedule("0 * * * *", handler);
