0e38ddeae1e019b6a0e5e2c507bdbf6fdef15810
Rory Flint <roryflintphoto@gmail.com>
2022-09-07 19:28:55 +0100

    chore: restructure to allow authorization app flow

---
+++ src/UnauthorizedApp.tsx
@@ -0,0 +1,12 @@
+function UnauthorizedApp(): JSX.Element {
+  return (
+    <div
+      className="max-w-[650px] my-0 mx-auto 
+    mt-[50px] px-[25px]"
+    >
+      <h1>Helo</h1>
+    </div>
+  );
+}
+
+export default UnauthorizedApp;
